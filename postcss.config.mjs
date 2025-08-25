36f95497a245346abdc91372fb2ceb58a0190d7e
patricia <garnierpatricia51@gmail.com>
2025-08-26 00:16:13 +0200

    taille

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 const config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // inclut tous tes fichiers source
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        45: "11.25rem", // correspond à px-45 ou w-45
+        50: "12.5rem",
+        90: "22.5rem",
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
